a74f774ae77039273dfbc2b14b62e0d0cf8c0711
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-14 14:08:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  name = name.pop("Ralph")
-  return name
+  kittens = kittens.pop("Ralph")
+  return kittens
 }
