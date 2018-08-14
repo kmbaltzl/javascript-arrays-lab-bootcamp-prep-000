ad24dfe3d7431db4a0ff7a3bbe20124ccb1c25d0
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-14 14:57:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function destructivelyPrependKitten(name) {
   }
   
   function removeLastKitten(name) {
-   kittens.slice(0, kittens.length-1)
-    return kittens
+   name.slice(0, name.length-1)
+    return name
   }
